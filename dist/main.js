459d3745337048a92573a767f559ac0cb0f75890
nonso01 <chukwunonsomartin01@gmail.com>
2023-01-01 08:51:53 +0100

    working on the header section

--- dist/main.js
+++ dist/main.js
@@ -2,3 +2,39 @@
 const d = document;
 const w = window;
 const log = console;
+const WAITASEC = 1000;
+const userInteractions = (function () {
+    on(".li:not(.ul li)", {
+        pointerover(e) {
+            var _a, _b;
+            const parent = e.composedPath()[0];
+            const child = parent.childNodes;
+            (_a = child[3]) === null || _a === void 0 ? void 0 : _a.classList.add("visible");
+            (_b = child[1]) === null || _b === void 0 ? void 0 : _b.classList.add("over");
+        },
+        pointerleave(e) {
+            var _a;
+            const parent = e.composedPath()[0];
+            const child = parent.childNodes;
+            (_a = child[1]) === null || _a === void 0 ? void 0 : _a.classList.remove("over");
+            on(".ul.visible", {
+                pointerleave(e) {
+                    var _a;
+                    (_a = e.composedPath()[0]) === null || _a === void 0 ? void 0 : _a.classList.remove("visible");
+                },
+            });
+        },
+    });
+})();
+function on(element, events) {
+    if (typeof element === "string") {
+        const e = d.querySelectorAll(element);
+        for (let key in events) {
+            e.forEach(function (el) {
+                el.addEventListener(key, events[key]);
+            });
+        }
+    }
+    else
+        log.warn("pls provide a css selector");
+}
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,44 @@
-const d = document
-const w = window
-const log = console
+const d = document;
+const w = window;
+const log = console;
+
+const WAITASEC = 1000;
+
+const userInteractions = (function () {
+  on(".li:not(.ul li)", {
+    pointerover(e: any) {
+      const parent = e.composedPath()[0];
+
+      const child = parent.childNodes;
+      child[3]?.classList.add("visible");
+      child[1]?.classList.add("over");
+      // log.log(child[1]);
+    },
+    pointerleave(e: any) {
+      const parent = e.composedPath()[0];
+      const child = parent.childNodes;
+      child[1]?.classList.remove("over");
+      on(".ul.visible", {
+        pointerleave(e: any) {
+          e.composedPath()[0]?.classList.remove("visible");
+        },
+      });
+    },
+  });
+})();
+
+/**
+ * on - adds multiple events to an element
+ * @param element = string
+ * @param events = object
+ */
+function on(element: string, events: {} | any): void {
+  if (typeof element === "string") {
+    const e = d.querySelectorAll(element);
+    for (let key in events) {
+      e.forEach(function (el) {
+        el.addEventListener(key, events[key]);
+      });
+    }
+  } else log.warn("pls provide a css selector");
+}
